5a7048a30c4a0b311deb07295029049c592a3cf2
Soniccooles <andrey.sonic@mail.ru>
2026-05-20 20:23:24 +0500

    feat (db): Added DbSet-s to context

--- src/DocANAI.Persistence/Context/PostgreSqlDbContext.cs
+++ src/DocANAI.Persistence/Context/PostgreSqlDbContext.cs
@@ -1,11 +1,30 @@
 ﻿using DocANAI.Persistence.Entities;
+using DocANAI.Persistence.Entities.AIModel;
+using DocANAI.Persistence.Entities.Answer;
+using DocANAI.Persistence.Entities.Format;
+using DocANAI.Persistence.Entities.Priority;
+using DocANAI.Persistence.Entities.ProcessingTask;
+using DocANAI.Persistence.Entities.Question;
+using DocANAI.Persistence.Entities.QuestionFile;
+using DocANAI.Persistence.Entities.SourceDocument;
+using DocANAI.Persistence.Entities.User;
 using Microsoft.EntityFrameworkCore;
 
 namespace DocANAI.Persistence.Context;
 
 public sealed class PostgreSqlDbContext(DbContextOptions<PostgreSqlDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<User> Users => Set<User>();
+    public DbSet<AIModel> AIModels => Set<AIModel>();
+    public DbSet<Format> Formats => Set<Format>();
+    public DbSet<PriorityLevel> PriorityLevels => Set<PriorityLevel>();
+    public DbSet<Priority> Priorities => Set<Priority>();
+    public DbSet<ProcessingTask> ProcessingTasks => Set<ProcessingTask>();
+    public DbSet<SourceDocument> SourceDocuments => Set<SourceDocument>();
+    public DbSet<QuestionFile> QuestionFiles => Set<QuestionFile>();
+    public DbSet<Question> Questions => Set<Question>();
+    public DbSet<Answer> Answers => Set<Answer>();
+    public DbSet<Report> Reports => Set<Report>();
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
